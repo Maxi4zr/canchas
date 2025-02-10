0eeca1d464858549a35c916f7d25a8d373feaaef
Maxi <maxiburgoa32@gmail.com>
2025-02-10 10:18:40 -0300

    FINALIZACION DE LA SECCION TURNOS

---
+++ pages/turnos.js
@@ -0,0 +1,21 @@
+document.getElementById('whatsapp').addEventListener('submit', function(event) {
+    event.preventDefault();  // Previene el envío del formulario
+
+    // Obtener los valores de los campos
+    var nombre = document.getElementById('nombre').value;   // Captura el valor del campo "Nombre"
+    var DNI = document.getElementById('DNI').value; // Captura el valor del campo "Mensaje"
+
+    // Crear el mensaje de WhatsApp (asegúrate de codificar los valores)
+    var whatsappMessage = encodeURIComponent(`Nombre: ${nombre}\nDNI: ${DNI}`);
+
+    // Número de teléfono de WhatsApp (incluyendo el código de país)
+    var phoneNumber = '542645848447'; // Reemplaza con el número de WhatsApp deseado
+
+    // Crear el enlace de WhatsApp
+    var whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
+
+    console.log(whatsappLink);
+
+    // Abrir WhatsApp en una nueva pestaña o ventana
+    window.open(whatsappLink, '_blank');
+});
