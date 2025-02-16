bd60baf33cebf60641584c11d7046997b1af05f6
Maxi <maxiburgoa32@gmail.com>
2025-02-16 12:05:33 -0300

    carpetas

---
+++ home.js
@@ -0,0 +1,17 @@
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
--- turnos.js
+++ turnos.js
@@ -4,13 +4,13 @@ document.getElementById('whatsapp').addEventListener('submit', function(event) {
     // Obtener los valores de los campos
     var nombre = document.getElementById('nombre').value;   // Captura el valor del campo "Nombre"
     var DNI = document.getElementById('DNI').value; // Captura el valor del campo "Mensaje"
+    var hora = document.getElementById('hora').value;
 
     // Crear el mensaje de WhatsApp (asegúrate de codificar los valores)
-    var whatsappMessage = encodeURIComponent(`Nombre: ${nombre}\nDNI: ${DNI}`);
+    var whatsappMessage = encodeURIComponent(`Nombre: ${nombre}\nDNI: ${DNI}\nHora: ${hora}`);
 
     // Número de teléfono de WhatsApp (incluyendo el código de país)
     var phoneNumber = '542645848447'; // Reemplaza con el número de WhatsApp deseado
-
     // Crear el enlace de WhatsApp
     var whatsappLink = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
 
